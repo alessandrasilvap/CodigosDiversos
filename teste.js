66ba94e27d2d5e8f8c84d96afb091bbe4fd45bcc
alessandrasilvap <alessandracris393@gmail.com>
2024-10-08 20:23:47 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,11 @@
+function toggleNews(element) {
+    const isExpanded = element.classList.contains('expanded');
+    
+    // Remove a expansão de todas as notícias
+    document.querySelectorAll('.news-item').forEach(item => item.classList.remove('expanded'));
+    
+    // Se não estava expandido, expande
+    if (!isExpanded) {
+        element.classList.add('expanded');
+    }
+}
