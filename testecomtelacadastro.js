2555a1dc4294162ffce1ca7f592ccb73f8bf174c
alessandrasilvap <alessandracris393@gmail.com>
2024-10-17 20:21:58 -0300

    Update testecomtelacadastro.js

--- testecomtelacadastro.js
+++ testecomtelacadastro.js
@@ -9,3 +9,15 @@ function confirmacaosenha() {
         return false;
     }
 }
+
+function mascaraTelefone(input) {
+    // Remove caracteres que não são dígitos
+    let valor = input.value.replace(/\D/g, '');
+    // Aplica a máscara
+    if (valor.length > 6) {
+        valor = valor.replace(/(\d{2})(\d{5})(\d)/, '($1) $2-$3');
+    } else if (valor.length > 2) {
+        valor = valor.replace(/(\d{2})(\d)/, '($1) $2');
+    }
+    input.value = valor;
+}
