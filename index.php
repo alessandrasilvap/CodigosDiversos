d3fea3c9690c05ed333ee8432285e455c30bcc43
alessandrasilvap <alessandracris393@gmail.com>
2025-03-10 20:33:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -29,14 +29,31 @@ $nota = 8.5;
 echo "<br> <br> Está variável é do tipo: ";
 echo gettype($nota);
 
+
 //Constante do php começa sempre com 'define'
 define("nome","Alessandra"); //Constante significa que o valor não vai mudar
 //Declarando a constante:
-echo "<br> <br> Constante nome: " . nome . "<b> <br>";
+echo "<br> <br> <h4>Constante nome: </h4>" . nome . "<br>";
 
 
+echo "<br> <h4> Minha lista </h4>";
 //Crindo um Array:
 $coisas = array("trabalho","faculdade","autoescola","investimentos","cursos","boxe","academia","surf");
 //Mostra a estrutura da lista Array
 var_dump($coisas);
+
+
+echo "<br> <br> <h4> Operações </h4>";
+//Operadores aritmétricos:
+$soma = 5 + 3;
+echo "<br> <br>Este resultado é " . $soma . "<br>";
+
+$soma1 = 7 - 2;
+echo "Este resultado é " . $soma1 . "<br>";
+
+$soma2 = 10 * 5;
+echo "Este resultado é " . $soma2 . "<br>";
+
+$soma3 = 10 / 2;
+echo "Este resultado é " . $soma3 . "<br>";
 ?>
