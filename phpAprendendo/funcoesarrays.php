f0f6a11d31460320e668c6aa37d689b641ec68aa
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-18 10:28:25 -0300

    Create funcoesarrays.php

---
+++ phpAprendendo/funcoesarrays.php
@@ -0,0 +1,12 @@
+<?php
+$nomes = array("Rodrigo", "Felipe", "Maria", "José");
+
+// IS_ARRAY($ARRAY) Verifica se uma determinada variável é um array
+if (is_array($nomes)) {
+  echo "É um array";
+} else {
+  echo "Não é um array";
+}
+
+//IN_ARRAY($VALOR, $ARRAY) Verifica se um determinado valor existe em alguma posição do array
+?>
