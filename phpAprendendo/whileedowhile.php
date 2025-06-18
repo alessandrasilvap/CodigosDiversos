0798df1dc655741bb3504bb945b4543389bf473b
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-18 12:22:48 -0300

    Create whileedowhile.php

---
+++ phpAprendendo/whileedowhile.php
@@ -0,0 +1,19 @@
+<?php
+//WHILE
+$contador = 1;
+
+while ($contador <= 10) {
+  echo "Contador é $contador <br>";
+  $contador++;
+}
+
+echo "<hr>";
+
+//DO WHILE
+$contador = 1;
+
+do {
+   echo "Contador é $contador <br>";
+  $contador++;
+} while ($contador <= 10);
+?>
