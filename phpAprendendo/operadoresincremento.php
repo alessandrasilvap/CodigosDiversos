20ff7007c27f60f6cfac2f7325545844ff9ab761
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-18 12:15:02 -0300

    Create operadoresincremento.php

---
+++ phpAprendendo/operadoresincremento.php
@@ -0,0 +1,19 @@
+<?php
+$valor = 20;
+
+//Pré-incremento
+echo ++$valor;
+
+//Pós-incremento
+echo $valor++;
+echo $valor;
+
+$valor1 = 20;
+
+//Pré-incremento
+echo --$valor1;
+
+//Pós-incremento
+echo $valor1--;
+echo $valor1;
+?>
