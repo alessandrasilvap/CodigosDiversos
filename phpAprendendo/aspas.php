db6a128bd34976c5757c1bf3200b706c4f9e4e5f
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-18 09:40:15 -0300

    Create aspas.php

---
+++ phpAprendendo/aspas.php
@@ -0,0 +1,6 @@
+<?php
+$nome = 'Alessandra Pereira';
+
+echo 'Meu nome é ' . $nome . ' e minha idade é \'20\''; //20 está entre barras e aspas simples para as aspas serem inseridas na mensagem
+echo '<hr>';
+echo "Meu nome é \"$nome\"";
