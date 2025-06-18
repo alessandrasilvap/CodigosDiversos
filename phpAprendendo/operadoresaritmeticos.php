f0ea5f27d964447f5502b116f602dd2882c2ffa4
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-18 12:05:43 -0300

    Create operadoresaritmeticos.php

---
+++ phpAprendendo/operadoresaritmeticos.php
@@ -0,0 +1,28 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+
+$adicao = $a + $b;
+echo $adicao;
+echo "<hr>";
+
+$subtracao = $c - $b;
+echo $subtracao;
+echo "<hr>";
+
+$multiplicacao = $d * $a;
+echo $multiplicacao;
+echo "<hr>";
+
+$modulo = $e % $d;
+echo $modulo;
+echo "<hr>";
+
+$expo = $a ** $d;
+echo $expo;
+echo "<hr>";
+
+echo 5 + 7 + 9 + 8 / 4;
+?>
