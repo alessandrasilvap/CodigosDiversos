7fb05d139c95ab3aefed02d189d63e5133677407
Alessandra Pereira <alessandracris393@gmail.com>
2025-06-23 11:36:50 -0300

    Update exercicio5.php

--- aulasSemestre2/exercicio5.php
+++ aulasSemestre2/exercicio5.php
@@ -1,3 +1,20 @@
+<html>
+<body>
 <?php
+define('Prestacoes', 5);
+$valorCompra = $_POST['valorCompra'];
+$numPrestacoes = $valorCompra / Prestacoes;
 
+echo "O valor da compra é $valorCompra e é disponibilizado " . Prestacoes . " , número de prestações do valor.";
 ?>
+
+
+
+
+
+<form action="" method="POST">
+  <input type="number" placeholder="Digite o valor: " name="valorCompra"><br>
+  <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
