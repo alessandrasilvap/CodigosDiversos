7865e8a275a81795580d5b903e636ceac5ebf215
Alessandra Pereira <alessandracris393@gmail.com>
2025-07-15 10:59:56 -0300

    Create conexao.php

---
+++ formulario/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$bd_name = "form";
+
+$conn = mysqli($servername, $username, $password, $bd_name);
+
+if ($conn->conect) {
+  
+?>
