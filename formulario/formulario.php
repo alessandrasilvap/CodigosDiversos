29f8b6a4f289ae556e6f5a755dfad4c9d7210589
Alessandra Pereira <alessandracris393@gmail.com>
2025-07-15 10:49:20 -0300

    Create formulario.php

---
+++ formulario/formulario.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+  <h1>Formulário</h1>
+  <br>
+  <p>Preencha os dados a seguir para entrar no sistema</p>
+  <br>
+  E-mail: <input type="email" name="email">
+  Senha: <input type="password" name="senha">
+  <input type="checkbox"> Lembrar e-mail
+  <br>
+  <input type="submit" value="Enviar">
+</body>
+</html>
