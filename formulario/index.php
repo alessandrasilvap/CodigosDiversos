daa941d5dfd69da9b9f2de13819156844bb3908b
Alessandra Pereira <alessandracris393@gmail.com>
2025-07-24 13:30:33 -0300

    Update index.php

--- formulario/index.php
+++ formulario/index.php
@@ -1,3 +1,10 @@
 <?php
 $enviar = isset($_POST['Enviar']) ? $_POST['Enviar'] : null;
+
+if ($enviar == 'Enviar') {
+  $stmt = $conn -> prepara ("SELECT nome, email, senha FROM usuario WHERE email = :email AND senha = :senha");
+
+  $stmt->bindParam(':email', $email,
+//aqui tem ue fazer as validações
+//sessão
 ?>
