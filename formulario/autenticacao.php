1eefe0519436dae8a59cb042cf595b5b3678057b
Alessandra Pereira <alessandracris393@gmail.com>
2025-07-27 02:31:28 -0300

    Update autenticacao.php

--- formulario/autenticacao.php
+++ formulario/autenticacao.php
@@ -1,5 +1,7 @@
 <?php
-session_strat();
-if(isset($_SESSION['usuario']) == false)
-  header('location:formulario.php');
+session_start();
+if(!isset($_SESSION['usuario'])) { //Verifica se a variável de sessão 'usuario' NÃO está definida
+    header('location: form.php'); //Redireciona para o formulário de login se não estiver logado
+    exit;
+}
 ?>
